0762f2a0bad1a1e78e91cf8df74fd86595f22e5e
reberet <124381326+reberet@users.noreply.github.com>
2025-11-18 04:34:34 -0300

    Actualizo README

---
+++ src/Library/Characters/Personaje.cs
@@ -0,0 +1,30 @@
+﻿namespace Library.Personajes; 
+{
+    // Esta clase va a aplicar para todos los personajes del juego, todos los personajes la van a usar de base
+    public abstract class Personaje
+    {
+        public string Nombre { get; set; }
+        private int SaludActual; // como lo dice el nombre
+        public int SaludMaxima { get; set; }
+        private List<IItem> items;
+
+        public int SaludActual
+        {
+            get => SaludActual;
+        }
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
